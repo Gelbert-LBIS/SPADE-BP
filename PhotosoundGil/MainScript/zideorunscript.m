8021b93831301ceb0bf888e3c266bf96406cc4e7
Gelbert-LBIS <gelbertgil@campus.technion.ac.il>
2026-05-24 15:48:24 +0300

    Delete PhotosoundGil directory

--- PhotosoundGil/MainScript/zideorunscript.m
+++
@@ -1,9 +0,0 @@
-% make video from zohar record
-
-matFiles = dir('*normal.mat');
-fileNames = {matFiles.name};
-disp(fileNames);
-
-for indx=1:length(fileNames)
-    ZoharMakeVideo(fileNames{indx});
-end
